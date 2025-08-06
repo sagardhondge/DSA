cdc623c7e9d6a40df1202eb8353a16db747fd34b
sagardhondge <sagardhondge56@gmail.com>
2025-08-07 00:29:19 +0530

    Loops in java

---
+++ src/sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a + b ;
+        System.out.println(c);
+//        int a =10;
+//        int b= 20;
+//        int c = a + b ;
+//        System.out.println(c);
+    }
+}
---
+++ src/weeks.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class weeks {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int day = in.nextInt();
+
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wenesday");
+                break;
+            case 4:
+                System.out.println("Tuesday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Sunday");
+        }
+    }
+}
