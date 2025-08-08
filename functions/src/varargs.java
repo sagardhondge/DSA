e298f4eb63e1471bc3d50ce609c1774afbafefe0
sagardhondge <sagardhondge56@gmail.com>
2025-08-08 20:52:07 +0530

    Loops in java

---
+++ functions/src/varargs.java
@@ -0,0 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class varargs {
+
+    public static void main(String[] args) {
+        fun(2,3,4,5,6,7,8);
+    }
+    static void fun(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+}
