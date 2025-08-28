0d24ef0c2992ff21653f5796c160fc7e280c3b9e
sagardhondge <sagardhondge56@gmail.com>
2025-08-28 15:49:32 +0530

    fuctions problems

---
+++ src/Arrays/arr.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class arr {
+    public static void main(String[] args) {
+
+        int [] a = {1, 2, 3, 4, 5};
+
+        System.out.println(Arrays.toString(a));
+    }
+}
