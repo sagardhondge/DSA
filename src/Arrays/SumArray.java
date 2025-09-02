390107cc7f469dad5a6d2aa399498c07e56788e2
sagardhondge <sagardhondge56@gmail.com>
2025-09-02 21:43:09 +0530

    Array

--- src/Arrays/SumArray.java
+++ src/Arrays/SumArray.java
@@ -1,4 +1,18 @@
 package Arrays;
 
 public class SumArray {
+    public static int sum(int[] arr) {
+        int total = 0;
+        for (int num : arr) {
+            total += num;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {10, 20, 30, 40};
+        int totalSum = sum(myArray);
+        System.out.println("Sum: " + totalSum);
+        // Output: Sum: 100
+    }
 }
